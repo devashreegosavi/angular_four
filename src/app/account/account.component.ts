71f9b796f1eaf80e859e6a326ff60f155fda9a4c
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2023-07-17 12:48:40 +0530

    17 july 2023
    1248pm

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,EventEmitter,Input} from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
-
+  @Input() account : {name:string, status:string};
+  @Input() id: number;
+  @Output() statusChanged = new EventEmitter<{id : number,newStatus:string }>();
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_four';
+
+  accounts=[
+    {
+      name : 'Master Account',
+      status : 'active'
+    },
+    {
+      name : 'Test Account',
+      status : 'Inactive'
+    },
+    {
+      name : 'Hidden Account',
+      status : 'Unknown'
+    }
+  ];
+
+  onAccountAdded(newAccount:{name:string,status:string}){
+    this.accounts.push(newAccount);
+  }
+  onStatusChanged(updateInfo : {id:number, newStatus:string}){
+    this.accounts[updateInfo.id].status=updateInfo.newStatus;
+  }
 }
