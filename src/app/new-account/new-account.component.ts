4369299630930cbb2401c1d557fdaa6cbed5921b
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2023-07-17 18:54:11 +0530

    17 july 2023
    654pm

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import {LoggingService } from '../logging.service'
+import { Component} from '@angular/core';
+import {LoggingService } from '../logging.service';
+import {AccountsService } from '../accounts.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
@@ -7,17 +8,15 @@ import {LoggingService } from '../logging.service'
   providers:[LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
-
-  constructor(private loggingServices : LoggingService){
+ 
+  constructor(private loggingServices : LoggingService,
+     private accountServices : AccountsService){
     
   }
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    
     //console.log('A server status changed, new status: ' + accountStatus);
     this.loggingServices.logStatusChange(accountStatus);
+    this.accountServices.addAccount(accountName,accountStatus);
   }
 }
